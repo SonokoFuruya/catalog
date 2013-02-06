59d6ef073d3619f51d30464bc7c0c1ad2c290d19
sonoko furuya <ofu.sonoko@gmail.com>
2013-02-06 10:34:44 +0900

    プロジェクトの新規作成

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catalog::Application.config.secret_token = '14ed61620b4d9a4ed296374c9cf1169773b3182e44d670d17fbbda10c57dd997ee48418193bdd7b67184daa5d204091cda555c9b1191162ad30a8bd80808ad74'
